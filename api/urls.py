e5918799086e27310b1dfc3e95f1e93065e4a9f0
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-07-14 15:48:19 +0600

    Created a place list endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import PlaceListView
+
+urlpatterns = [
+    path("places/", PlaceListView.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Place
+from .serializers import PlaceSerializer
+
+
+class PlaceListView(APIView):
+    def get(self, request):
+        places = Place.objects.all()
+        serializer = PlaceSerializer(places, many=True)
+        return Response({"places": serializer.data})
+
+    def post(self, request):
+        serializer = PlaceSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
